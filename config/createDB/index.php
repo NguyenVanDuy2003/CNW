7e125038e30ff2c5c7d2dc377e5dfbaa5b7e4349
duynguyen <duynguyen.starack@gmail.com>
2023-09-30 22:26:50 +0700

    feat: create sign in and sign up

---
+++ config/createDB/index.php
@@ -0,0 +1,26 @@
+<?php
+    include "../connectSQL/index.php";
+    $query = "SHOW TABLES LIKE 'users'";
+    $result = $db->query($query);
+
+    if ($result->num_rows > 0) {
+        echo "Da co db";
+    } else {
+        $sql = 'CREATE TABLE users (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            name VARCHAR(255),
+            username VARCHAR(255),
+            password VARCHAR(255),
+            address VARCHAR(255),
+            createAt VARCHAR(255),
+            updateAt VARCHAR(255),
+            email VARCHAR(255)
+        )';
+        if ($db->query($sql) === TRUE) {
+            echo "New record created successfully";
+        } else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+        $db->close();
+    }
+?>
